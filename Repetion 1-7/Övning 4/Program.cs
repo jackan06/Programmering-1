870a6ec6dfc9fe0283f2763be6490d4074e6d3cc
jackan06 <152333943+jackan06@users.noreply.github.com>
2023-11-28 22:23:06 +0100

    Add files via upload

---
+++ Repetion 1-7/Övning 4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange ett heltal:");
+            int heltal = int.Parse(Console.ReadLine());
+
+            if (heltal%2 == 0)
+            {
+                Console.WriteLine("Det är ett jämnt tal");
+            }
+            else
+            {
+                Console.WriteLine("Det är inte ett jämnt tal");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
