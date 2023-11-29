99217f3dddeb484e2fdb1749e18badf6227b8663
jackan06 <152333943+jackan06@users.noreply.github.com>
2023-11-29 12:25:42 +0100

    Add files via upload

---
+++ C#/Kap 7-9/Kap 9/Exempel(nästlade loopar)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exempel_nästlade_loopar_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for(int j=1; j <= 5; j++)
+                {
+                    Console.Write(i*j+"\t");
+                }
+                Console.WriteLine();
+                //Console.Write("\n");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Kap 7-9/Kap 9/Extra uppgift9_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extra_uppgift9_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.Write(j*i + "\t");
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
