df491e9a7850fb9ce8ecc4b23bb7e0e51bc4e812
jackan06 <152333943+jackan06@users.noreply.github.com>
2023-11-29 12:24:43 +0100

    Add files via upload

---
+++ C#/Kap 7-9/Kap 9/Exempel(Fält)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exempel_Fält_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Slumpar 
+            int[] slumpadeTal = new int[100];
+            Random random = new Random();
+            //slumpadeTal[0]=random.Next(1, 7);
+            //slumpadeTal[1] = random.Next(1, 7);
+            //slumpadeTal[2] = random.Next(1, 7);
+            //Console.WriteLine(slumpadeTal[0]);
+
+            for (int i = 0; i < slumpadeTal.Length; i++)
+            {
+                slumpadeTal[i] = random.Next(1, 7);
+                Console.Write(slumpadeTal[i] + "\t");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
