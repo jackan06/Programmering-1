615de4b0981d5371c52042c92bf483db466cd49c
jackan06 <152333943+jackan06@users.noreply.github.com>
2023-12-05 22:00:11 +0100

    Add files via upload

---
+++ C#/Kap 7-9/Repetion kap 9/Test uppgift decimaltal/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_uppgift_decimaltal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett decimaltal");
+            string decimaltal = Console.ReadLine();
+            int summa = 0;
+            int[] tal = new int[decimaltal.Length];
+            bool felInmatning = false;
+
+            for (int i = 0; i < decimaltal.Length && felInmatning==false; i++)
+            {
+
+                if (((int)decimaltal[i] - 48) >= 0 && ((int)decimaltal[i] - 48) <= 9)
+                {
+                    tal[i] = ((int)decimaltal[i]-48);
+                    summa += ((int)decimaltal[i]-48);
+                }
+                else if (decimaltal[i]!=','&& decimaltal[i] != '.' || ((int)decimaltal[i] - 48) >= 9)
+                {
+                    felInmatning=true;
+                }
+
+            }
+
+            if (felInmatning==false)
+            {
+                Console.WriteLine(summa);
+            }
+            else
+            {
+                Console.WriteLine("Fel inmatning");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Kap 7-9/Repetion kap 9/Test uppgift(fält)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_uppgift_fält_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många gånger ska det köras");
+            int antal= int.Parse(Console.ReadLine());
+            int[] omvandlat = new int[antal];
+            int tal = 5;
+            int summa = 0;
+           
+
+            for (int i=0; i<antal; i++)
+            {
+                tal *= 2;
+                tal += 4;
+                summa += tal;
+                omvandlat[i] = tal;
+            }
+
+            Console.WriteLine(summa);
+
+            Console.ReadKey();
+        }
+    }
+}
